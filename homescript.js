846aec211d16f90a136a27b7472dde6f84691cfd
Justus2004 <jus.bernard.23@lehre.mosbach.dhbw.de>
2024-06-21 08:22:14 +0200

    Anpassung

---
+++ homescript.js
@@ -0,0 +1,31 @@
+let slideIndex = 0;
+        const slidesWrapper = document.querySelector('.slides-wrapper');
+        const dots = document.querySelectorAll('.dot');
+        const totalSlides = document.querySelectorAll('.mySlides').length;
+
+        function plusSlides(n) {
+            slideIndex += n;
+            if (slideIndex > totalSlides - 3) {
+                slideIndex = 0;
+            } else if (slideIndex < 0) {
+                slideIndex = totalSlides - 3;
+            }
+            showSlides();
+        }
+
+        function currentSlide(n) {
+            slideIndex = n - 1;
+            showSlides();
+        }
+
+        function showSlides() {
+            slidesWrapper.style.transform = `translateX(-${slideIndex * 33.33}%)`;
+            dots.forEach((dot, index) => {
+                dot.className = dot.className.replace(" active", "");
+                if (index === slideIndex) {
+                    dot.className += " active";
+                }
+            });
+        }
+
+        showSlides();
